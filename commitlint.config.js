4aca56e87fa7dd0382285d17cf40d7426ca0fcb4
hugoer <hugoerdev@gmail.com>
2026-03-31 01:03:34 +0200

    build: add commitlint for standardized commit message formatting

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test']],
+        'scope-enum': [1, 'always', ['lab', 'rum', 'collect', 'sitemap', 'links', 'cli', 'prompts', 'profiles', 'utils']],
+        'scope-empty': [0],
+        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+        'subject-full-stop': [2, 'never', '.'],
+        'header-max-length': [2, 'always', 100],
+    },
+};
