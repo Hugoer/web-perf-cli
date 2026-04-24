d17e47c67ade33d52950a8abf9a8d125504d1181
hugoer <hugoerdev@gmail.com>
2026-04-24 22:22:51 +0200

    fix: update JSDoc for runLinks function to include return type and correct links structure

--- types/lib/links.d.ts
+++ types/lib/links.d.ts
@@ -1,10 +1,11 @@
 /**
  * @param {string} url
+ * @returns {Promise<{ outputPath: string, links: Array<{ href: string, text: string }> }>}
  */
 export function runLinks(url: string): Promise<{
     outputPath: string;
-    links: {
-        href: any;
+    links: Array<{
+        href: string;
         text: string;
-    }[];
+    }>;
 }>;
